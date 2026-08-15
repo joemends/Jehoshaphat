1fe12b66ac77a6d2afde33c1a76708d12365cc17
joemends <joemends217@gmail.com>
2026-08-15 16:11:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
    4. Animation speed is controlled here. Smaller number = faster.
    ========================================================= */
 const SITE_CONFIG = {
-  supabaseUrl: 'https://bdhxswgkwelkvvwgqywu.supabase.co/rest/v1/',
+  supabaseUrl: 'https://bdhxswgkwelkvvwgqywu.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkaHhzd2drd2Vsa3Z2d2dxeXd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NDYzNDksImV4cCI6MjEwMjMyMjM0OX0.O54DqOStDNFR1gW4uNqiojFbWJibp3MGVa3ZT1YpnYg',
   animations: {
     revealDuration: 700,
